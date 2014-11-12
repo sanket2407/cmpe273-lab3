028b0dee96dd77e672767933b9bd6cbfe220b2c7
sanket2407 <sanket24.07@gmail.com>
2014-11-12 17:28:42 +0530

    Update Client.java

--- client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
+++ client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
@@ -18,10 +18,11 @@ public class Client {
         char[] values = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
     	
         //Putting data
+        //md5 hash function
         for(int i=0; i<10; i++)
         {
-        	int bucket = Hashing.consistentHash(Hashing.md5().hashString(Integer.toString(i+1)), link.size()); //md5 hash function is used.
-        	link.get(bucket).put(i, Character.toString(values[i])); //i = key
+        	int bucket = Hashing.consistentHash(Hashing.md5().hashString(Integer.toString(i+1)), link.size()); 
+        	link.get(bucket).put(i, Character.toString(values[i])); 
         	System.out.println("http://localhost:300" + bucket + " put key-value pair " + i+1 +"->"+ values[i]);
         }
     
@@ -29,7 +30,7 @@ public class Client {
         for(int j=0; j<10; j++)
         {
         	int bucket1 = Hashing.consistentHash(Hashing.md5().hashString(Integer.toString(j+1)), link.size());
-        	link.get(bucket1).get(j+1); //j = key
+        	link.get(bucket1).get(j+1);
         	System.out.println("http://localhost:300" + bucket1 + " get key-Value pair " + j+1 +"->"+ link.get(bucket1).get(j+1));
 
         }
