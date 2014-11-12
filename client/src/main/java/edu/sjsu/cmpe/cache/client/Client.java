e22df2935f13d7b51b2864bdf7cf55cd2bc6678d
sanket2407 <sanket24.07@gmail.com>
2014-11-12 16:58:53 +0530

    Client.java updated by sanket
    Client.java updated by sanket

--- client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
+++ client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
@@ -1,19 +1,38 @@
 package edu.sjsu.cmpe.cache.client;
 
+import java.util.List;
+import java.util.ArrayList;
+import com.google.common.hash.Hashing;
+
 public class Client {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Starting Cache Client...");
-        CacheServiceInterface cache = new DistributedCacheService(
-                "http://localhost:3000");
+        
+         System.out.println("Starting Cache Client…\n");
+         
+        List<CacheServiceInterface> link = new ArrayList<CacheServiceInterface>();
+        link.add(new DistributedCacheService("http://localhost:3000"));
+        link.add(new DistributedCacheService("http://localhost:3001"));
+        link.add(new DistributedCacheService("http://localhost:3002"));
+         
+        char[] values = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
+    	
+        //Putting data
+        for(int i=0; i<10; i++)
+        {
+        	int bucket = Hashing.consistentHash(Hashing.md5().hashString(Integer.toString(i+1)), link.size()); //md5 hash function is used.
+        	link.get(bucket).put(i, Character.toString(values[i])); //i = key
+        	System.out.println("http://localhost:300" + bucket + " put key-value pair " + i+1 +"->"+ values[i]);
+        }
+    
+        //getting data
+        for(int j=0; j<10; j++)
+        {
+        	int bucket1 = Hashing.consistentHash(Hashing.md5().hashString(Integer.toString(j+1)), link.size());
+        	link.get(bucket1).get(j+1); //j = key
+        	System.out.println("http://localhost:300" + bucket1 + " get key-Value pair " + j+1 +"->"+ link.get(bucket1).get(j+1));
 
-        cache.put(1, "foo");
-        System.out.println("put(1 => foo)");
-
-        String value = cache.get(1);
-        System.out.println("get(1) => " + value);
-
-        System.out.println("Existing Cache Client...");
+        }
     }
 
 }
